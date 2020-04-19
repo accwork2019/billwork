df03608aa97da3772e03a8c421cd903e3422cf55
Bivash Kanti Pal <bivashpal2007@gmail.com>
2020-04-19 20:31:04 +0530

    history

---
+++ .history/config/db-config_20200419200541.js
@@ -0,0 +1,11 @@
+// DATABASE Config files
+
+var config_value = [];
+
+config_value['user']      = 'root';
+config_value['password']  = '';
+config_value['host']      = 'localhost';
+config_value['port']      = '3306';
+config_value['database']  = 'acpanel';
+
+module.exports = config_value;
